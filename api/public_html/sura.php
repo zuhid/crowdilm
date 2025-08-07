247381c1e676e6792db00438b3285bc5192914d4
Tanvir Ather <tanvir.ather@zuhid.com>
2025-08-06 20:54:18 -0600

    fix sura

--- api/public_html/sura.php
+++ api/public_html/sura.php
@@ -1,6 +1,6 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/../private_html/initialize.php');
 
-$sql = "SELECT id,name FROM table_name;";
+$sql = "SELECT id,ayas,name_arabic,name_english,revelation_city,revelation_order FROM sura;";
 echo select($sql, true);
 ?>
